754a34ba9fea209da37a3c03daccf654e837f28d
dvsidorov <dvsidorov88@mail.ru>
2015-11-17 11:16:31 +0500

    url patches

--- urls.py
+++ urls.py
@@ -6,6 +6,7 @@ from .views import CatalogView
 urlpatterns = patterns(
     'catalog.views',
     # url(r'slash/$', 'slash', name = 'slash'),
+    url(r'^tovar/(?P<tovar_id>.*)/$', 'tovar_nocat_inside', name='tovar_nocat_inside'),
     url(r'(?P<catalog_id>.*)/(?P<tovar_id>.*)/$', 'tovar_inside', name='tovar_inside'),
     # url(r'(.*)/$', 'category_inside', name='catalog_inside'),
     # url(r'$', 'category_list', name='catalog_list'), )
