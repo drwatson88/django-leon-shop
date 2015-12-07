d54368c06e4f5e67f9516edb46d413275e1a03c8
dvsidorov <dvsidorov88@mail.ru>
2015-12-08 02:02:16 +0500

    call

--- context_processors.py
+++ context_processors.py
@@ -2,7 +2,9 @@
 
 from models import Category
 
+
 def categorys(request):
+
     try:
         return {'catalog': Category.get_root_nodes()[0].get_children().filter(show=True)}
     except Exception:
